61ceb0afdb8b3761242512360cd9305f44fe9d07
Haya abu al halawa <hayaah@wix.com>
2019-05-09 13:03:41 +0300

    Add QuillJS

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
 import '../statics/editor.css';
+import 'react-quill/dist/quill.bubble.css';
 
-export default function Editor(props) {
-  return (
-    <div className="Editor_container">
-      <textarea className="Editor" />
-    </div>
-  );
-};
+class Editor extends React.Component {
+
+  render() {
+    return (
+      <ReactQuill
+        theme='bubble'
+        modules={{
+          toolbar: false,
+        }}
+        toolbar={false}
+        className='Editor_container'
+      >
+        <div
+          key="editor"
+          ref="editor"
+          className="quill-contents Editor"
+        />
+      </ReactQuill>
+    );
+  }
+}
+
+export default Editor;
