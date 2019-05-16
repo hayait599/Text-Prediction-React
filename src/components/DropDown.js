25c23ba29ede50706e72bca0ec30367b7cf16f1f
Haya abu al halawa <hayaah@wix.com>
2019-05-16 12:18:56 +0300

    Add DropDown Item selection ( using tab and mouse click )

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -5,7 +5,7 @@ class DropDown extends React.Component {
 
   constructor(props) {
     super(props)
-    this.dropDownRef = React.createRef();
+    this.dropDownRef = React.createRef()
     this.onKeyUp = this.onKeyUp.bind(this)
     this.onKeyDown = this.onKeyDown.bind(this)
     this.moveStep = this.moveStep.bind(this)
@@ -16,40 +16,44 @@ class DropDown extends React.Component {
   }
 
   componentDidMount() {
-    const { distanceToBottom, distanceToTop, bounds, hintsArray } = this.props.properties;
+    const { distanceToBottom, distanceToTop, bounds, hintsArray } = this.props.properties
     for (let i = 0; i < hintsArray.length; i++) {
-      let rowItem = document.createElement('div');
-      rowItem.hint = hintsArray[i];
-      rowItem.innerHTML = `<div>${hintsArray[i]}</div>`;
-      this.rows.push(rowItem);
-      this.dropDownRef.appendChild(rowItem);
+      let rowItem = document.createElement('div')
+      rowItem.id = i
+      rowItem.hint = hintsArray[i]
+      rowItem.innerHTML = `<div>${hintsArray[i]}</div>`
+      rowItem.onmousedown = () => this.onWordSelection(true)
+      rowItem.onmouseover = () => this.onMouseHover(i)
+      this.rows.push(rowItem)
+      this.dropDownRef.appendChild(rowItem)
     }
 
     if (distanceToTop > distanceToBottom * 3) {
-      this.dropDownRef.style.maxHeight = `${distanceToTop}px`;
-      this.dropDownRef.style.top = `${bounds.top}px`;
-      this.dropDownRef.style.left = `${bounds.left + 2}px`;
+      this.dropDownRef.style.maxHeight = `${distanceToTop}px`
+      this.dropDownRef.style.top = `${bounds.top}px`
+      this.dropDownRef.style.left = `${bounds.left + 2}px`
     } else {
-      this.dropDownRef.style.top = `${bounds.top}px`;
-      this.dropDownRef.style.left = `${bounds.left + 2}px`;
-      this.dropDownRef.style.maxHeight = `${distanceToBottom}px`;
+      this.dropDownRef.style.top = `${bounds.top}px`
+      this.dropDownRef.style.left = `${bounds.left + 2}px`
+      this.dropDownRef.style.maxHeight = `${distanceToBottom}px`
     }
+    this.highLightRow(0)
   }
 
   highLightRow(index) {
     if (this.oldIndex !== -1 && this.rows[this.oldIndex]) {
-      this.rows[this.oldIndex].style.backgroundColor = '#ffffff';
+      this.rows[this.oldIndex].style.backgroundColor = '#ffffff'
     }
-    this.rows[index].style.backgroundColor = '#e5e5e5';
-    this.oldIndex = index;
+    this.rows[index].style.backgroundColor = '#e5e5e5'
+    this.oldIndex = index
   }
 
   moveStep(step) {
     if (this.index + step === -1 || this.index + step === this.rows.length) {
-      return this.rows[this.index];
+      return this.rows[this.index]
     }
-    this.index += step;
-    this.highLightRow(this.index);
+    this.index += step
+    this.highLightRow(this.index)
     this.selectedRow = this.index
   }
 
@@ -61,13 +65,27 @@ class DropDown extends React.Component {
     this.moveStep(-1)
   }
 
+  onMouseHover(index) {
+    this.selectedRow = index
+    this.highLightRow(index)
+  }
+
+  onWordSelection(addSpace) {
+    const word = this.rows[this.selectedRow].hint
+    if (addSpace) {
+      this.props.addHint(` ${word}`)
+    } else {
+      this.props.addHint(word)
+    }
+  }
+
   render() {
     return (
       <div
         className="DropDown"
         ref={elm => this.dropDownRef = elm}
       />
-    );
+    )
   }
 }
 
