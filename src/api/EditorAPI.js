5cf76ba27c0407e58242548066f814ca715956e7
Haya abu al halawa <hayaah@wix.com>
2019-05-21 11:57:36 +0300

    Remove console

--- src/api/EditorAPI.js
+++ src/api/EditorAPI.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 class Api {
 
   getHints({ hintText }) {
-    console.log(hintText)
     if (hintText.length !== 0 && hintText && hintText !== 'undefined') {
       return axios.get(`https://protected-mesa-89217.herokuapp.com/api/${hintText}/5/5`, {
         method: 'HEAD',
